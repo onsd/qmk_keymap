26b49d3a72b8a8a10f282d9d976930ab41da8915
Taka Omori <31408@toyota.kosen-ac.jp>
2018-05-15 20:49:51 +0900

    add quote

--- keymap.c
+++ keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------|   |-------------+------+------+------+------+------|
  * | ctrl |   A  |   S  |   D  |   F  |   G  |  (   |   |  )   |   H  |   J  |   K  |   L  |   ;  |Enter |
  * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
- * | Sft  |   Z  |   X  |   C  |   V  |   B  |  {   |   |  }   |   N  |   M  |   ,  |   .  |   /  |Shift |
+ * | Sft  |   Z  |   X  |   C  |   V  |   B  |     |   |  }   |   N  |   M  |   ,  |   .  |   /  |Shift |
  * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
  * |MO      | MO   | TG   | TG   |GUI   | ESC |Space|   |Space | Click|Click |      |      | MO   |MO   |
  * |  meta  | symb |  meta|  SYMB|      |      |    |   |      |  left| right| -    |      | symb |meta |
@@ -38,12 +38,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   TD(TD_TAB_ESC),   KC_Q,    KC_W,   KC_E,    KC_R,             KC_T,          KC_LBRC,       KC_RBRC,    KC_Y,             KC_U,    KC_I,     KC_O,     KC_P,     KC_BSPC, \
   KC_LCTRL,  KC_A,    KC_S,   KC_D,    KC_F,             KC_G,          S(KC_9),       S(KC_0),    KC_H,             KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_ENT, \
   KC_LSFT,  KC_Z,    KC_X,   KC_C,    KC_V,             KC_B,          S(KC_LBRC),    S(KC_RBRC), KC_N,             KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT, \
- MO(META), MO(SYMB),TG(META), TG(SYMB) , KC_LGUI,        KC_ESC,        KC_SPC,        KC_SPC,    KC_MS_BTN1,     KC_MS_BTN2, KC_MINS, KC_MS_D,  TG(SYMB), TG(META) \
+ MO(META), MO(SYMB),TG(META), TG(SYMB) , KC_LGUI,        KC_ESC,        KC_SPC,        KC_SPC,    KC_MS_BTN1,     KC_MS_BTN2, KC_MINS, KC_MS_D,  MO(SYMB), MO(META) \
 ),
 
 /* META
  * ,------------------------------------------------.   ,------------------------------------------------.
- * |      |   1  |   2  |   3  |   4  |   5  |   6  |   |   7  |   8  |   9  |   0  |   -  |  =   |  |   |
+ * |  ~ |   1  |   2  |   3  |   4  |   5  |   6  |   |   7  |   8  |   9  |   0  |   -  |  =    |  |   |
  * |------+------+------+------+------+------+------|   |-------------+------+------+------+-----+------|
  * |      |  F1  |      |Muhen | Henk |      |  (   |   |  )   | Left | Down |  Up  |Right |      |      |
  * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
@@ -55,8 +55,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [META] = KEYMAP( \
-  _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,  KC_0,    KC_MINS, KC_EQL, S(KC_BSLS),   \
-  _______, KC_F1,   XXXXXXX, KC_MHEN, KC_HENK, XXXXXXX, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, XXXXXXX, XXXXXXX, \
+  KC_GRAVE, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,  KC_0,    KC_MINS, KC_EQL, S(KC_BSLS),   \
+  _______, KC_F1,   XXXXXXX, KC_MHEN, KC_HENK, XXXXXXX, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_QUOTE, XXXXXXX, \
   _______, KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   _______, _______, KC_F7,   KC_F8,  KC_F9, KC_F10,  KC_F11,  SFT_T(KC_RO), \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET, _______, _______, _______ \
 ),
@@ -75,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [SYMB] = KEYMAP( \
-   _______,S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), S(KC_6), S(KC_7), S(KC_8), S(KC_9),    S(KC_0),   S(KC_MINS), S(KC_EQL), _______, \
+   _______,S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), S(KC_6), S(KC_7), S(KC_8), S(KC_9),    S(KC_0),   S(KC_MINS), S(KC_EQL),KC_QUOT, \
   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, KC_MS_L,KC_MS_D,   KC_MS_U,KC_MS_R,   S(KC_SCLN), S(KC_QUOT), \
   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX, S(KC_COMM), S(KC_DOT), S(KC_SLSH), S(KC_RO), \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______,   _______,    _______ \
